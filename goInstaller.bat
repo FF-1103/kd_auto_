c3a5791813dd9e6f8fa36883b5f43ff363a19429
fanzhishuo <332021128@qq.com>
2026-02-12 11:20:31 +0800

    优化

--- goInstaller.bat
+++ goInstaller.bat
@@ -1,2 +1,2 @@
 chcp 65001
-pyinstaller -F run.py --add-data "config;config" --add-binary "chromedriver.exe;." --console --name "运单号自动化工具" --clean --distpath D:\dist
+pyinstaller -F main.py --add-data "config;config" --add-binary "chromedriver.exe;." --console --name "运单号自动化工具" --clean --distpath D:\dist
